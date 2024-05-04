655d4218f06b801e05a220cd9d697bc670a288d1
Emerson <emersonr8@hotmail.com>
2024-05-04 13:03:56 -0300

    Concluido cadastro de produtos

---
+++ src/main/java/set/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,79 @@
+package main.java.set.Ordenacao;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProdutos {
+
+    // atributo
+    private Set<Produto> produtoSet;
+
+    /**
+     * Metodo Construtor
+     */
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    /**
+     * Metodo para adicionar novo produto no Set
+     * 
+     * @param cod
+     * @param nome
+     * @param preco
+     * @param quantidade
+     */
+    public void adicionarProduto(long cod, String nome, double preco, int quantidade) {
+        produtoSet.add(new Produto(cod, nome, preco, quantidade));
+    }
+
+    /**
+     * Metodo que retorna o set de produtos organizados por nome
+     * 
+     * @return Set<Produto>
+     */
+    public Set<Produto> exibirProdutosPorNome() {
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        if (!produtoSet.isEmpty()) {
+            return produtosPorNome;
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+    }
+
+    /**
+     * Metodo que retorna o set de produtos organizados por preco
+     * 
+     * @return Set<Produto>
+     */
+    public Set<Produto> exibirProdutosPorPreco() {
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparetorPorPreco());
+        if (!produtoSet.isEmpty()) {
+            produtosPorPreco.addAll(produtoSet);
+            return produtosPorPreco;
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Criando uma instância do CadastroProdutos
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+        // Adicionando produtos ao cadastro
+        cadastroProdutos.adicionarProduto(1L, "Smartphone", 1000d, 10);
+        cadastroProdutos.adicionarProduto(2L, "Notebook", 1500d, 5);
+        cadastroProdutos.adicionarProduto(1L, "Mouse", 30d, 20);
+        cadastroProdutos.adicionarProduto(4L, "Teclado", 50d, 15);
+
+        // Exibindo todos os produtos no cadastro
+        System.out.println(cadastroProdutos.produtoSet);
+
+        // Exibindo produtos ordenados por nome
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+
+        // Exibindo produtos ordenados por preço
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+    }
+}
