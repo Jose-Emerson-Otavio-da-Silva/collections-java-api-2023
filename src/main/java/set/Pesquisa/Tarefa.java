94bbdf0c130101285beb75d19690982674e206df
Emerson <emersonr8@hotmail.com>
2024-05-04 23:18:36 -0300

    Concluido lista de tarefas

---
+++ src/main/java/set/Pesquisa/Tarefa.java
@@ -0,0 +1,42 @@
+package main.java.set.Pesquisa;
+
+public class Tarefa {
+    // atributos
+    private String descricao;
+    private boolean concluida;
+
+    /**
+     * Metodo construtor
+     * 
+     * @param descricao
+     */
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+
+    // getters
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{" +
+                "descricao='" + descricao + '\'' +
+                ", concluida=" + concluida +
+                '}';
+    }
+}
